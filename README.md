01133feb33a2ca325439e6395865d279c5eef03f
nityapatel19 <nityapatel19@gmail.com>
2023-10-06 09:27:28 +0530

    Added minimax mode and restructured code

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # tic-tac-toe
-The classic game of Tic Tac Toe with Easy, Medium and Multiplayer mode.
+The classic game of Tic Tac Toe with Easy, Medium, Hard and Multiplayer mode.
+
+## Game Modes
+- **Easy:** The computer will make random moves.
+- **Medium:** The computer will try to find a winning move or stop you from making a winning move. If no immediate winning move is found, it will make a random move.
+- **Hard:** The computer will make the most optimal move based on the minimax algorithm.
+- **Multiplayer:** Play against a friend.
 
 ## Requirements
-<li> Python 3.10 </li>
+- Python
--- main.py
+++ main.py
@@ -1,142 +1,184 @@
-# importing enum for enumerations
-from enum import Enum
-from typing import List
 import random
 
 
-class Status(Enum):
-    DRAW = 0
-    PLAYER_ONE = 1
-    PLAYER_TWO = 2
+def insert_letter(board, letter, pos):
+    board[pos] = letter
 
 
-SIZE = 3
-solutions = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+def space_is_free(board, pos):
+    return board[pos] == ' '
 
 
-def print_grid(grid: List[str]):
-    print(f"""
- {grid[0]} | {grid[1]} | {grid[2]} 
------------
- {grid[3]} | {grid[4]} | {grid[5]} 
------------
- {grid[6]} | {grid[7]} | {grid[8]} 
-""")
+def print_board(board):
+    print("""
+     {} | {} | {} 
+    -----------
+     {} | {} | {} 
+    -----------
+     {} | {} | {} 
+    """.format(*board[1:]))
 
 
-def check_winner(grid: List[str]):
-    if any(all([grid[i] == 'X' for i in s]) for s in solutions):
-        return Status.PLAYER_ONE
-    elif any(all([grid[i] == 'O' for i in s]) for s in solutions):
-        return Status.PLAYER_TWO
-    elif ' ' not in grid:
-        return Status.DRAW
-    else:
-        return None
+def is_board_full(board):
+    return board.count(' ') == 0
 
 
-def take_turn(grid: List[str], player: str, inp: List[int]) -> Status:
-    grid[inp[0] * 3 + inp[1]] = player
-    return check_winner(grid)
+def is_winner(board, letter):
+    return any(all([board[pos] == letter for pos in trio]) for trio in WINNING_TRIOS)
 
 
-def validate_input(grid: List[str], s: str):
-    s = list(map(int, s.strip().split()))
-    if all([i in range(3) for i in s]) and len(s) == 2 and grid[s[0] * 3 + s[1]] == ' ':
-        return s
-    else:
-        return False
+def player_move(board, letter):
+    while True:
+        try:
+            move = int(input("Enter position (1-9): "))
+            if move not in range(1, 10):
+                print("Position out of range. Try again.")
+            elif not space_is_free(board, move):
+                print("Position already occupied. Try again.")
+            else:
+                insert_letter(board, letter, move)
+                break
+        except ValueError:
+            print("Position not a number. Try again.")
+
+
+def computer_move_easy(board):
+    empty_spaces = [i for i in range(1, 10) if space_is_free(board, i)]
+    move = random.choice(empty_spaces)
+    insert_letter(board, 'O', move)
+
+
+def computer_move_medium(board):
+    possible_moves = {i for i in range(1, 10) if space_is_free(board, i)}
+
+    # Check if computer can win in the next move, if yes, make that move
+    # Or if player can win in the next move, block that move
+    for letter in ['O', 'X']:
+        for move in possible_moves:
+            board_copy = board[:]
+            insert_letter(board_copy, letter, move)
+            if is_winner(board_copy, letter):
+                insert_letter(board, 'O', move)
+                return
+
+    open_corners = list(possible_moves.intersection([1, 3, 7, 9]))
+    if open_corners:
+        move = random.choice(open_corners)
+        insert_letter(board, 'O', move)
+        return
+
+    if 5 in possible_moves:
+        insert_letter(board, 'O', 5)
+        return
+
+    open_edges = list(possible_moves.intersection([2, 4, 6, 8]))
+    if open_edges:
+        move = random.choice(open_edges)
+        insert_letter(board, 'O', move)
+        return
+
+
+def minimax(board, maximize=True):
+    letter = 'O' if maximize else 'X'
+    win_score = 1 if maximize else -1
+
+    if is_winner(board, letter):
+        return win_score
+    elif is_board_full(board):
+        return 0
+
+    possible_moves = {i for i in range(1, 10) if space_is_free(board, i)}
+    best_score = float('-inf') if maximize else float('inf')
+    for move in possible_moves:
+        board[move] = letter
+        score = minimax(board, not maximize)
+        board[move] = ' '
+        best_score = max(score, best_score) if maximize else min(score, best_score)
+
+    return best_score
+
+
+def computer_move_hard(board):
+    possible_moves = {i for i in range(1, 10) if space_is_free(board, i)}
+    best_score = float('-inf')
+    best_move = None
+    for move in possible_moves:
+        board[move] = 'O'
+        # O wants to maximize. O makes a move and checks the best minimized score that X can give.
+        # Then chooses the maximum from those.
+        score = minimax(board, maximize=False)
+        board[move] = ' '
+        if score > best_score:
+            best_score = score
+            best_move = move
+    insert_letter(board, 'O', best_move)
 
 
 def player_vs_player():
-    grid = [' '] * SIZE * SIZE
-    winner = None
-    player = ['X', 'O']
-    while winner is None:
-        print_grid(grid)
+    print("PLAYER VS PLAYER")
+    print("Player 1 is X and Player 2 is O.")
 
-        while not (
-                inp := validate_input(grid, input(
-                    f'Player {player[0]}, take your turn (Enter coordinates separated by spaces): '))):
-            print("Invalid Input. Try again:")
+    board = ['#'] + [' '] * 9
+    print_board(board)
 
-        winner = take_turn(grid, player[0], inp)
-        player.append(player.pop(0))
-
-    print_grid(grid)
-    if winner.name == "DRAW":
-        print("The game is a draw.")
-    else:
-        print(f"{winner.name} wins!!!")
-
-
-def player_vs_computer_easy():
-    grid = [' '] * SIZE * SIZE
-    empty_places = ["0 0", "0 1", "0 2", "1 0", "1 1", "1 2", "2 0", "2 1", "2 2"]
-    winner = None
-    player = ['X', 'O']
-    while winner is None:
-        print_grid(grid)
-
-        if player[0] == 'X':
-            choice = input(f'Player {player[0]}, take your turn (Enter coordinates separated by spaces): ')
-            while not (turn := validate_input(grid, choice)):
-                choice = input("Invalid Input. Try again: ")
-
+    while True:
+        if not is_winner(board, 'O'):
+            player_move(board, 'X')
+            print_board(board)
         else:
-            choice = random.choice(empty_places)
-            turn = validate_input(grid, choice)
+            print("Player 2 won!!!")
+            break
 
-        winner = take_turn(grid, player[0], turn)
-        empty_places.remove(choice)
-        player.append(player.pop(0))
+        if is_board_full(board):
+            print("The game is a draw.")
+            break
 
-    print_grid(grid)
-    if winner.name == "DRAW":
-        print("The game is a draw.")
-    elif winner == Status.PLAYER_ONE:
-        print("Congratulations, you won!!!")
-    else:
-        print("You lost, better luck next time.")
-
-
-def player_vs_computer_medium():
-    # grid = [' '] * SIZE * SIZE
-    # empty_places = ["0 0", "0 1", "0 2", "1 0", "1 1", "1 2", "2 0", "2 1", "2 2"]
-    # winner = None
-    # player = ['X', 'O']
-    # while winner is None:
-    #     print_grid(grid)
-    #
-    #     if player[0] == 'X':
-    #         choice = input(f'Player {player[0]}, take your turn (Enter coordinates separated by spaces): ')
-    #         while not (turn := validate_input(grid, choice)):
-    #             choice = input("Invalid Input. Try again: ")
-    #
-    #     else:
-    #         if
-    ...
+        if not is_winner(board, 'X'):
+            player_move(board, 'O')
+            print_board(board)
+        else:
+            print("Player 1 won!!!")
+            break
 
 
-def player_vs_computer_hard():
-    ...
+def player_vs_computer(level=1):
+    computer_move = COMPUTER_MOVE[level]
+    print("PLAYER VS COMPUTER")
+    print("You are X and Computer is O.")
+
+    board = ['#'] + [' '] * 9
+    print_board(board)
+
+    while True:
+        if not is_winner(board, 'O'):
+            player_move(board, 'X')
+        else:
+            print("You lost!")
+            break
+
+        if is_board_full(board):
+            print("The game is a draw.")
+            break
+
+        if not is_winner(board, 'X'):
+            computer_move(board)
+            print_board(board)
+        else:
+            print_board(board)
+            print("You won!!!")
+            break
 
 
-def player_vs_computer():
-    level = input("Enter level of difficulty (1-3), anything else to quit: ")
-    if level == '1':
-        player_vs_computer_easy()
-    elif level == '2':
-        player_vs_computer_medium()
-    elif level == '3':
-        player_vs_computer_hard()
-    else:
-        print("Invalid Input.")
+WINNING_TRIOS = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7))
+COMPUTER_MOVE = {
+    1: computer_move_easy,
+    2: computer_move_medium,
+    3: computer_move_hard
+}
 
 
 if __name__ == '__main__':
     print("Welcome to Tic-Tac-Toe!")
-    print("Player 1 is X and Player 2 is O")
     play = input("Press p to play, anything else to quit: ").lower()
     while play == 'p':
         mode = input("Press 1 for Player vs Player, 2 for Player vs Computer: ")
@@ -145,7 +187,15 @@ if __name__ == '__main__':
             play = input("Press p to play again, q to quit: ").lower()
 
         elif mode == '2':
-            player_vs_computer()
+            try:
+                level = int(input("Enter level of difficulty (1-3), anything else to quit: "))
+                if 1 <= level <= 3:
+                    player_vs_computer(level)
+                else:
+                    break
+            except ValueError as e:
+                print(e)
+                break
             play = input("Press p to play again, anything else to quit: ").lower()
 
         else:
